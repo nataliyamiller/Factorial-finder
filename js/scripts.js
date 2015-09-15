b57ff538f42713a0f4075e6982a0f13a581ee405
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-14 17:08:47 -0700

    Passes specs for finding a factorial with a recursive function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var factorial = function(number) {
   if (number === 0 || number === 1) {
     return 1;
+  } else if (number > 1) {
+    return number * factorial(number - 1);
   } else {
     return false;
   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe('factorial', function() {
   it("is false for a number that is not a factorial of a given number", function() {
-    expect(factorial(120)).to.equal(false);
+    expect(factorial(-2)).to.equal(false);
   });
 
   it("is 1 for number 0", function() {
@@ -9,6 +9,10 @@ describe('factorial', function() {
 
   it("is 1 for number 1", function() {
     expect(factorial(1)).to.equal(1);
+  });
+
+  it("is 120 for number 5", function() {
+    expect(factorial(5)).to.equal(120);
   });
 
 });
